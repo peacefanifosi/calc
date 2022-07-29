b6d9c4a7a348e04417a548f06c10d0c7f7441188
afolabi <afolabimuideen15@gmail.com>
2022-07-29 15:31:34 +0100

    calculator-project

---
+++ script.js
@@ -0,0 +1,113 @@
+function getvalue1(){
+
+    document.getElementById('result').value +='1'
+    
+    }
+    
+    function getvalue2(){
+    
+        document.getElementById('result').value +='2'
+        
+        }
+
+        function getvalue3(){
+    
+            document.getElementById('result').value +='3'
+            
+            }
+
+            function getvalue4(){
+    
+                document.getElementById('result').value +='4'
+                
+                }
+
+                function getvalue5(){
+    
+                    document.getElementById('result').value +='5'
+                    
+                    }
+
+                    function getvalue6(){
+    
+                        document.getElementById('result').value +='6'
+                        
+                        }
+
+                        function getvalue7(){
+    
+                            document.getElementById('result').value +='7'
+                            
+                            }
+
+        function getvalue8(){
+    
+                             document.getElementById('result').value +='8'
+                                
+                                }
+         function getvalue9(){
+    
+                             document.getElementById('result').value +='9'
+                                    
+                                    }
+        function getvaluez(){
+    
+                            document.getElementById('result').value +='0'
+                                        
+                                        }
+
+
+         function getvaluemultiply(){
+    
+                                    document.getElementById('result').value +='*'
+                                            
+                                            }
+         function getvaluedivide(){
+    
+                                   document.getElementById('result').value +='/'
+                                                
+                                                }
+
+         function getvalueopenbracket(){
+    
+                                        document.getElementById('result').value +='('
+                                                    
+                                                    }
+
+        function getvalueclosebracket(){
+    
+                                        document.getElementById('result').value +=')'
+                                                        
+                                                        }
+
+        function getvaluesubtract(){
+    
+                                     document.getElementById('result').value +='-'
+                                                            
+                                                            }
+
+         function getvaluedecimalpoint(){
+    
+                                            document.getElementById('result').value +='.'
+                                                                
+                                                                }
+
+        function getvalueadd(){
+    
+                                 document.getElementById('result').value +='+'
+                                                                    
+                                                                    }
+
+        function getvaluepercent(){
+    
+                                    document.getElementById('result').value +='%'
+                                                                        
+                                                                        }
+
+         function getresult(){
+                                document.getElementById('result').value = eval(document.getElementById('result').value)
+                                                                            
+                                                             }
+         function getclear(){
+            document.getElementById('result').value = ''
+         }                                                    
